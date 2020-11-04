7731686af3d90e01348705e5e57490950ef20746
Maximilian Bauregger <maximilian.bauregger@studium.uni-hamburg.de>
2020-11-05 00:04:41 +0100

    Rename previous/current to old/new in check.py

--- check.py
+++ check.py
@@ -39,14 +39,14 @@ def get_data():
     return requests.get(api_endpoint).json()
 
 
-def textify_change(state_name, previous, current):
-    prev_cast = previous['votes_cast']
-    prev_all = previous['votes_all']
-    prev_c = previous['candidates']
+def textify_change(state_name, old, new):
+    prev_cast = old['votes_cast']
+    prev_all = old['votes_all']
+    prev_c = old['candidates']
 
-    curr_cast = current['votes_cast']
-    curr_all = current['votes_all']
-    curr_c = current['candidates']
+    curr_cast = new['votes_cast']
+    curr_all = new['votes_all']
+    curr_c = new['candidates']
     biden_ahead = (curr_c['Joe Biden']['votes'] > curr_c['Donald Trump']['votes'])
 
     candidate_delta = abs(curr_c['Joe Biden']['votes'] - curr_c['Donald Trump']['votes'])
@@ -57,29 +57,29 @@ A new vote count for {state_name}.
 Biden gained {curr_c['Joe Biden']['votes'] - prev_c['Joe Biden']['votes']:,} votes.
 Trump gained {curr_c['Donald Trump']['votes'] - prev_c['Donald Trump']['votes']:,} votes.
 
-The current situation in {state_name} is as follows
-Biden: {curr_c['Joe Biden']['votes']:,} votes ({curr_c['Joe Biden']['votes']/curr_cast * 100: 3.2f}%)
-Trump: {curr_c['Donald Trump']['votes']:,} votes ({curr_c['Donald Trump']['votes']/curr_cast * 100: 3.2f}%)
+The current situation in {state_name} is as follows:
+Biden has {curr_c['Joe Biden']['votes']:,} votes ({curr_c['Joe Biden']['votes']/curr_cast * 100: 3.2f}%)
+Trump has {curr_c['Donald Trump']['votes']:,} votes ({curr_c['Donald Trump']['votes']/curr_cast * 100: 3.2f}%)
 
 {"Biden" if biden_ahead else "Trump"} is ahead of {"Trump" if biden_ahead else "Biden"} by {candidate_delta:,} votes.
 
-votes casted: {curr_cast:,} / {curr_all:,} ({prev_cast/prev_all * 100: 3.2f}%)
+Votes casted: {curr_cast:,} / {curr_all:,} ({prev_cast/prev_all * 100: 3.2f}%)
 """
 
 
 if __name__ == '__main__':
 
-    previous = parse_data(get_data())
+    old = parse_data(get_data())
 
     while True:
 
-        current = parse_data(get_data())
+        new = parse_data(get_data())
         changes = False
 
         for k in d.keys() & battlegrounds:
-            if previous[k]['votes_cast'] != current[k]['votes_cast']:
-                textify_change(previous[k], current[k])
-                previous[k] = current[k]
+            if old[k]['votes_cast'] != new[k]['votes_cast']:
+                textify_change(old[k], new[k])
+                old[k] = new[k]
                 changes = True
 
         if not changes:
